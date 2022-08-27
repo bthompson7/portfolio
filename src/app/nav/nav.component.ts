af1382334fdb04abca3af9a5baa5ca38e2018249
Ben Thompson <splitoe@gmail.com>
2022-08-27 00:39:06 -0400

    menu button icon changes when its clicked

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, QueryList, Renderer2, ViewChild, ViewChildren } from '@angular/core';
 
 @Component({
   selector: 'app-nav',
@@ -7,27 +7,41 @@ import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angula
 })
 export class NavComponent implements OnInit, AfterViewInit {
 
-  // html elements
+  // html elements we need to change
   @ViewChild('siteNavSide') siteNavSide: ElementRef;
-  @ViewChild('navBtn') navBtn: ElementRef;
+  @ViewChild('navBtn') navButton: ElementRef;
 
-  constructor() {
+  constructor(private renderer: Renderer2) {
+
   }
 
   ngOnInit(): void {
+
   }
 
   ngAfterViewInit() {
-    this.navBtn.nativeElement.onclick = () => {
-      var siteNavSideBtn = this.siteNavSide.nativeElement.style.display;
+    let navBtnIcon = this.navButton.nativeElement.children[0];
+
+    this.navButton.nativeElement.onclick = () => {
+      let siteNavSideBtn = this.siteNavSide.nativeElement.style.display;
       if (siteNavSideBtn !== 'flex') {
+
+        // change icon css class
+        this.renderer.removeClass(navBtnIcon, "fa-solid");
+        this.renderer.removeClass(navBtnIcon, "fa-bars");
+        this.renderer.addClass(navBtnIcon, "fa-times");
+
+        // menu css
         this.siteNavSide.nativeElement.style.display = 'flex';
         this.siteNavSide.nativeElement.style.justifyContent = 'center';
       } else {
         this.siteNavSide.nativeElement.style.display = 'none';
-      }
 
+        // change icon class
+        this.renderer.removeClass(navBtnIcon, "fa-times");
+        this.renderer.addClass(navBtnIcon, "fa-solid");
+        this.renderer.addClass(navBtnIcon, "fa-bars");
+      }
     }
   }
-
 }
