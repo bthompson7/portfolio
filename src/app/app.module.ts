144deb9b879db31a6dc9fdb112decfc155b3d0ec
Ben Thompson <35780502+bthompson7@users.noreply.github.com>
2024-11-16 12:33:53 -0500

    update to angular 18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,23 +4,16 @@ import { HomeComponent } from './home/home.component';
 import { ExperienceComponent } from './experience/experience.component';
 import { SkillsComponent } from './skills/skills.component';
 import { ProjectsComponent } from './projects/projects.component';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { BackendService } from './service/backend.service';
 import { AboutComponent } from './about/about.component';
 
-@NgModule({
-  declarations: [
-    HomeComponent,
-    SkillsComponent,
-    ExperienceComponent,
-    ProjectsComponent,
-    AboutComponent
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule
+@NgModule({ declarations: [
+        HomeComponent,
+        SkillsComponent,
+        ExperienceComponent,
+        ProjectsComponent,
+        AboutComponent
     ],
-  providers: [BackendService],
-  bootstrap: [HomeComponent]
-})
+    bootstrap: [HomeComponent], imports: [BrowserModule], providers: [BackendService, provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
